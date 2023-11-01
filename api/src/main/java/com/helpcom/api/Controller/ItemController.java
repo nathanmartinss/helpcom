174578b93f0c52a2a123243240fc5a71aa3a75a2
Nathan Martins <naa.limaa@hotmail.com>
2023-10-31 21:04:12 -0300

    refactored POST and added GET method

--- api/src/main/java/com/helpcom/api/Controller/ItemController.java
+++ api/src/main/java/com/helpcom/api/Controller/ItemController.java
@@ -1,17 +1,28 @@
 package com.helpcom.api.Controller;
 
-import com.helpcom.api.Item.NovoItemDTO;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.helpcom.api.Item.Item;
+import com.helpcom.api.Item.ItemDTO;
+import com.helpcom.api.Item.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("item")
 public class ItemController {
-    @PostMapping
-    public void adicionarItem(@RequestBody NovoItemDTO dados) {
-        System.out.println(dados);
 
+    @Autowired
+    private ItemRepository repository;
+    @PostMapping
+    @Transactional
+    public void adicionarItem(@RequestBody ItemDTO dados) {
+        repository.save(new Item(dados));
+    }
+
+    @GetMapping
+    public List<ItemDTO> listarItems () {
+        return repository.findAll().stream().map(ItemDTO :: new).toList();
     }
 }
