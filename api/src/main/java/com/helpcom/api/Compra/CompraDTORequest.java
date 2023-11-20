2916cf477ac5ba931817d6d25d04171c042b69e3
Ronaldo Thame <ronaldothame1@gmail.com>
2023-11-20 20:52:11 -0300

    changed Date to LocalDate for better usability

--- api/src/main/java/com/helpcom/api/Compra/CompraDTORequest.java
+++ api/src/main/java/com/helpcom/api/Compra/CompraDTORequest.java
@@ -1,10 +1,9 @@
 package com.helpcom.api.Compra;
 
-import com.helpcom.api.Item.Item;
 
-import java.util.Date;
+import java.time.LocalDate;
 
-public record CompraDTORequest(Long idItem, Integer qtdItem, Integer valor, Date validade, Date dataCompra) {
+public record CompraDTORequest(Long idItem, Integer qtdItem, Integer valor, LocalDate validade, LocalDate dataCompra) {
     public CompraDTORequest(Compra compra) {
         this(compra.getIdItem(), compra.getQtdItem(), compra.getValor(), compra.getValidade(), compra.getDataCompra());
     }
--- api/src/main/java/com/helpcom/api/Compra/CompraDTOResponse.java
+++ api/src/main/java/com/helpcom/api/Compra/CompraDTOResponse.java
@@ -1,10 +1,9 @@
 package com.helpcom.api.Compra;
 
-import com.helpcom.api.Item.Item;
 
-import java.util.Date;
+import java.time.LocalDate;
 
-public record CompraDTOResponse(Long id, Long idItem, Integer qtdItem, Integer valor, Date validade, Date dataCompra) {
+public record CompraDTOResponse(Long id, Long idItem, Integer qtdItem, Integer valor, LocalDate validade, LocalDate dataCompra) {
     public CompraDTOResponse(Compra compra) {
         this(compra.getId(), compra.getIdItem(), compra.getQtdItem(), compra.getValor(), compra.getValidade(), compra.getDataCompra());
     }
